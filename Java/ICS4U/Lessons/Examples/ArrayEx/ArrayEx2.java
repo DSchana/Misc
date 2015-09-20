eb18ddf4c9f13ae41e2a67ef942062399b6aafd6
dilpreetC <iibrownguyii@gmail.com>
2015-09-20 12:07:09 -0400

    Update

---
+++ Java/ICS4U/Lessons/Examples/ArrayEx/ArrayEx2.java
@@ -0,0 +1,16 @@
+// ArrayEx2.java
+
+import java.util.*
+
+public class ArrayEx2 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String sent = kb.nextLine();
+		String[] words = sent.split();
+		String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		
+		for (String word : words) {
+			
+		}
+	}
+}
