d9f6d4b7bc648200257d604020343745b286bba3
Limeabean <kimvan963@hotmail.com>
2015-10-02 11:47:06 -0400

    Update Basic1.java

--- Java/ICS4U/Lessons/Basic/Basic1.java
+++ Java/ICS4U/Lessons/Basic/Basic1.java
@@ -1,17 +1,15 @@
 // Basic1.java
-
+//remember extra spaces are UGGLYY, comon 
 public class Basic1 {
 	public static void main(String[] args) {
 		/*
 		Variables in Java are statically types.
 		This means we declare variables by giving them a type
 		*/
-
 		int x = 10;
 		double money = 50.25;
 		char grade = 'A';
 		String name = "Aaron";
-
 		/*
 		int, double and char are primative types
 		String is class
