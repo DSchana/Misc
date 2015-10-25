b0a1d3eaf6366dbd5ee020645f5ad164218740a5
dilpreetC <dilpreet_chana@hotmail.com>
2015-10-25 18:55:57 -0400

    Recursion assignment 2

--- Java/ICS4U/Assignments/Recursion/Recursion2.java
+++ Java/ICS4U/Assignments/Recursion/Recursion2.java
@@ -9,6 +9,8 @@ public class Recursion2 {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		int[][] grid = new int[8][8];
+		/*
 		int[][] grid = {
 			{ 0, 12, 7, 43, 32, 12, 30, 15 },
 			{ 21, 26, 18, 34, 41, 9, 17, 21 },
@@ -19,14 +21,13 @@ public class Recursion2 {
 			{ 35, 15, 12, 21, 24, 26, 14, 35 },
 			{ 15, 12, 18, 25, 14, 22, 21, 15 }
 		};
+		*/
 
-		/*
 		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; i++) {
-				grid[i][j] = kb.nextInt();
+			for (int j = 0; j < 8; j++) {
+				grid[j][i] = kb.nextInt();
 			}
 		}
-		*/ 
 
 		findPath(grid);
 
@@ -38,15 +39,16 @@ public class Recursion2 {
 	}
 
 	public static void findPath(int[][] grid, String path, int x, int y, int time) {
-		if (x == 7 && y == 7) {
-			if (time < best_time) {
+		if (x == 7 || y == 8) {
+			if (time < best_time && path.length() >= 14) {
 				best_time = time;
 				best_path = path;
 			}
 		}
 		else {
-			findPath(grid, path + "D", x, y+1, time + grid[x][y]);
-			findPath(grid, path + "R", x+1, y, time + grid[x][y]);
+			findPath(grid, path + "R", x+1, y, time + grid[y][x]);
+			findPath(grid, path + "D", x, y+1, time + grid[y][x]);
+
 		}
 	}
 }
