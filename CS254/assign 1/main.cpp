bac7535d3bf750faa6b31dd486b7cc737dc0605a
Dilpreet Chana <dschana6@gmail.com>
2017-09-25 01:32:45 -0400

    CS254 assugn done

--- CS254/assign 1/main.cpp
+++ CS254/assign 1/main.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <cstdio>
 #include <string>
+#include <iostream>
 #include <vector>
 #include <climits>
 
@@ -10,7 +12,7 @@ struct Post {
 	int comments;
 	int size;
 	int date;
-	string name;
+	char name[100];
 };
 
 void showPosts(vector<Post> posts, bool conditional);
@@ -44,15 +46,15 @@ int main() {
 			break;
 		case 3:
 			// Show number of posts
-			printf("There are %d posts currently stored\n", posts.size());
+			printf("There are %d posts currently stored\n", (int)posts.size());
 			break;
 		case 4:
 			// Add new post
-			editPostList(*posts, true);
+			editPostList(&posts, true);
 			break;
 		case 5:
 			// Delete post by
-			editPostList(*posts, false);
+			editPostList(&posts, false);
 			break;
 		case 6:
 			posts.clear();
@@ -65,7 +67,7 @@ int main() {
 			printf("4. Post size\n");
 			printf("Choose one of the above as a sorting condition: ");
 			scanf("%d", &userIn);
-			sort(*posts, userIn);
+			sort(&posts, userIn);
 
 			break;
 		case 8:
@@ -82,16 +84,15 @@ int main() {
 void showPosts(vector<Post> posts, bool conditional = false) {
 	if (conditional) {
 		int userIn, target;
-		string s_target;
+		char s_target[100];
 
 		printf("1. Number of likes\n");
 		printf("2. Number of comments\n");
 		printf("3. Date posted\n");
 		printf("4. Post size\n");
-		printf("5. Name");
+		printf("5. Name\n");
 		printf("Choose one of the above as a sorting condition: ");
 		scanf("%d", &userIn);
-		sort(*posts, userIn);
 
 		switch (userIn) {
 		case 1:
@@ -99,50 +100,60 @@ void showPosts(vector<Post> posts, bool conditional = false) {
 			scanf("%d", &target);
 			for (int i = 0; i < posts.size(); i++) {
 				if (posts[i].likes == target) {
-					printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+					printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 					break;
 				}
 			}
+			printf("No records found.\n");
+
 			break;
 		case 2:
 			printf("Enter the number of comments: ");
 			scanf("%d", &target);
 			for (int i = 0; i < posts.size(); i++) {
 				if (posts[i].comments == target) {
-					printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+					printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 					break;
 				}
 			}
+			printf("No records found.\n");
+
 			break;
 		case 3:
 			printf("Enter the post date: ");
 			scanf("%d", &target);
 			for (int i = 0; i < posts.size(); i++) {
 				if (posts[i].date == target) {
-					printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+					printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 					break;
 				}
 			}
+			printf("No records found.\n");
+
 			break;
 		case 4:
 			printf("Enter the size of the post: ");
 			scanf("%d", &target);
 			for (int i = 0; i < posts.size(); i++) {
 				if (posts[i].size == target) {
-					printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+					printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 					break;
 				}
 			}
+			printf("No records found.\n");
+
 			break;
 		case 5:
 			printf("Enter the name: ");
-			scanf("%s", &s_target);
+			scanf("%s", s_target);
 			for (int i = 0; i < posts.size(); i++) {
 				if (posts[i].name == s_target) {
-					printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+					printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 					break;
 				}
 			}
+			printf("No records found.\n");
+
 			break;
 		default:
 			printf("Invalid Option\n");
@@ -151,7 +162,7 @@ void showPosts(vector<Post> posts, bool conditional = false) {
 	}
 	else {
 		for (int i = 0; i < posts.size(); i++) {
-			printf("%s posted a %d character post on %d. It received %d likes and %d comments.", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
+			printf("%s posted a %d character post on %d. It received %d likes and %d comments.\n", posts[i].name, posts[i].size, posts[i].date, posts[i].likes, posts[i].comments);
 		}
 	}
 }
@@ -161,36 +172,36 @@ void editPostList(vector<Post> *posts, bool addition) {
 		Post n_post;
 
 		printf("Enter name: ");
-		scanf("%s", n_post->name);
+		scanf("%s", n_post.name);
 
 		printf("Enter likes: ");
-		scanf("%d", n_post->likes);
+		scanf("%d", &n_post.likes);
 
 		printf("Enter comments: ");
-		scanf("%d", n_post->comments);
+		scanf("%d", &n_post.comments);
 
 		printf("Enter size of post in number of characters: ");
-		scanf("%d", n_post->size);
+		scanf("%d", &n_post.size);
 
-		printf(Enter date of post in the form YYYYMMDD);
-		scanf("%d", n_post->date);
+		printf("Enter date of post in the form YYYYMMDD: ");
+		scanf("%d", &n_post.date);
 
-		posts->push_back(n_post);
+		(*posts).push_back(n_post);
 	}
 	else {
 		int target, userIn;
-		string s_target;
+		char s_target[100];
 
-		printf("Delete by 1. author or 2. date?");
+		printf("Delete by 1. author or 2. date: ");
 		scanf("%d", &userIn);
 
 		if (userIn == 1) {
 			printf("Enter name: ");
-			scanf("%s", &s_target);
+			scanf("%s", s_target);
 
-			for (int i = 0; i < posts->size(); i++) {
+			for (int i = 0; i < (*posts).size(); i++) {
 				if ((*posts)[i].name == s_target) {
-					posts->erase(posts->begin() + i);
+					(*posts).erase((*posts).begin() + i);
 				}
 			}
 		}
@@ -198,9 +209,9 @@ void editPostList(vector<Post> *posts, bool addition) {
 			printf("Enter date in the form of YYYYMMDD: ");
 			scanf("%d", &target);
 
-			for (int i = 0; i < posts->size(); i++) {
-				if ((*posts)[i].name == target) {
-					posts->erase(posts->begin() + i);
+			for (int i = 0; i < (*posts).size(); i++) {
+				if ((*posts)[i].date == target) {
+					(*posts).erase((*posts).begin() + i);
 				}
 			}
 		}
@@ -211,36 +222,64 @@ void editPostList(vector<Post> *posts, bool addition) {
 }
 
 void sort(vector<Post> *posts, int condition) {
-	// Eh maybe just do with bubble sort. easy winz.
-	int max = INT_MIN;
-	int min = INT_MAX;
+	bool swapped = true;
 
 	switch (condition) {
 	case 1:
 		// Likes
-		for (int i = 0; i < posts->size(); i++) {
-			max = (*posts)[i].likes > max ? (*posts)[i].likes : max;
-			min = (*posts)[i].likes < min ? (*posts)[i].likes : min;
+		while (swapped) {
+			swapped = false;
+
+			for (int i = 0; i < (*posts).size() - 1; i++) {
+				if ((int)(*posts)[i].likes > (int)(*posts)[i + 1].likes) {
+					iter_swap(posts->begin() + i, posts->begin() + i + 1);
+					swapped = true;
+				}
+			}
 		}
 
-		int radix[max - min];
-
-		for (int i = 0; i < max - min; i++) {
-			radix[i] = 0;
-		}
-
-		for (int i = 0; i < posts->size(); i++) {
-			radix[(*posts)[i].likes - min]++;
-		}
 		break;
 	case 2:
 		// Comments
+		while (swapped) {
+ 			swapped = false;
+
+			for (int i = 0; i < (*posts).size() - 1; i++) {
+				if ((*posts)[i].comments > (*posts)[i + 1].comments) {
+					iter_swap(posts->begin() + i, posts->begin() + i + 1);
+					swapped = true;
+				}
+			}
+		}
+
 		break;
 	case 3:
 		// Date
+		while (swapped) {
+			swapped = false;
+
+			for (int i = 0; i < (*posts).size() - 1; i++) {
+				if ((*posts)[i].date > (*posts)[i + 1].date) {
+					iter_swap(posts->begin() + i, posts->begin() + i + 1);
+					swapped = true;
+				}
+			}
+		}
+
 		break;
 	case 4:
 		// Size
+		while (swapped) {
+			swapped = false;
+
+			for (int i = 0; i < (*posts).size() - 1; i++) {
+				if ((*posts)[i].size > (*posts)[i + 1].size) {
+					iter_swap(posts->begin() + i, posts->begin() + i + 1);
+					swapped = true;
+				}
+			}
+		}
+
 		break;
 	default:
 		break;
