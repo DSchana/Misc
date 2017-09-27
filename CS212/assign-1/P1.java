dd20e6350caca4ac37aed37263f5c4f88a0b2022
Dilpreet Chana <dschana6@gmail.com>
2017-09-27 04:05:50 -0400

    Everything is shit

---
+++ CS212/assign-1/P1.java
@@ -0,0 +1,11 @@
+class P1 {
+	public static void main(String[] args) {
+		double val = 1000;
+		double interest = 0.05;
+
+		for (int i = 0; i < 3; i++) {
+			val *= 1 + interest;
+			System.out.printf("year %d: $%.2f\n", i + 1, val);
+		}
+	}
+}
---
+++ CS212/assign-1/P2.java
@@ -0,0 +1,5 @@
+public class P2 {
+	public static void main(String[] args) {
+
+	}
+}
