7fe431341c554ad9b3973f00dad4ca2e0563879d
Dilpreet S. Chana <dschana6@gmail.com>
2017-10-21 20:33:52 -0400

    Save domething

--- CS212/Shit.java
+++ CS212/Shit.java
@@ -2,10 +2,6 @@ import java.awt.Rectangle;
 
 public class Shit {
 	public static void main(String[] args) {
-		Rectangle rect = new Rectangle(10, 20, 30, 25);
-		System.out.println("Area: " + (rect.getWidth() * rect.getHeight()) + " Paremeter: " + (2 * rect.getWidth() + 2 * rect.getHeight()));
-		rect.translate(10, 20);
-		rect.setSize(35, 10);
-		System.out.println("Area: " + (rect.getWidth() * rect.getHeight()) + " Paremeter: " + (2 * rect.getWidth() + 2 * rect.getHeight()));
+		System.out.printf("%d\n", "COMP".compareTo("comp"));
 	}
 }
