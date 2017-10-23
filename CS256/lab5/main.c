622d96461ef755de772eaf6623fae487cdb719e1
Dilpreet S. Chana <dschana6@gmail.com>
2017-10-23 17:12:27 -0400

    sys lab 5

--- CS256/lab5/main.c
+++ CS256/lab5/main.c
@@ -1,13 +1,42 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int arg_c, char **arg_v) {
-	pid_t p1 = fork();
-
-	// Second fork in parent only
-	if (p1 > 0) {
-		pid_t p2 = fork();
+int main(int argc, char *argv[]){
+	int pid;
+	int processNum = 1;
+	
+	for (int i = 1;i < 3;i++){
+		if (processNum == 1){
+			if ((pid = fork()) < 0){
+				exit(1);
+			}
+		}
+		if (pid == 0){
+			if (processNum == 1){
+				processNum = i + 1;
+			}
+			if (processNum == 2 || processNum == 3){
+				for (int j = 1;j < 3;j++){
+					if (processNum == 2 || processNum == 3){
+						if ((pid = fork()) < 0){
+							exit(1);
+						}
+					}
+					if (pid == 0){
+						if (processNum == 2 || processNum == 3){
+							if (j == 1){
+								processNum = i + j + 2;
+							}
+							else if (j == 2){
+								processNum = i + j + 3;
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-
-	return 0;
+	sleep(atoi(argv[processNum]));
+	printf("process %d terminated pid=%d\n", processNum,getpid());
 }
