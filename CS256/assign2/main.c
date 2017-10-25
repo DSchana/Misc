043e692e72bbd19117d9bee7d70cc2edddb56e16
Dilpreet S. Chana <dschana6@gmail.com>
2017-10-25 18:33:19 -0400

    Save

--- CS256/assign2/main.c
+++ CS256/assign2/main.c
@@ -29,39 +29,39 @@ int main(int arg_c, char** arg_v) {
 	for (int i = 0; i < 5; i++) {
 		if (strcmp(arg_v[i + 2], "red") == 0) {
 			colours[i] = (char*)malloc(sizeof(RED));
-			colours[i] = RED;
+			strcpy(colours[i], RED);
 		}
 		else if (strcmp(arg_v[i + 2], "green") == 0) {
 			colours[i] = (char*)malloc(sizeof(GREEN));
-			colours[i] = GREEN;
+			strcpy(colours[i], GREEN);
 		}
 		else if (strcmp(arg_v[i + 2], "blue") == 0) {
 			colours[i] = (char*)malloc(sizeof(BLUE));
-			colours[i] = BLUE;
+			strcpy(colours[i], BLUE);
 		}
 		else if (strcmp(arg_v[i + 2], "yellow") == 0) {
 			colours[i] = (char*)malloc(sizeof(YELLOW));
-			colours[i] = YELLOW;
+			strcpy(colours[i], YELLOW);
 		}
 		else if (strcmp(arg_v[i + 2], "orange") == 0) {
 			colours[i] = (char*)malloc(sizeof(ORANGE));
-			colours[i] = ORANGE;
+			strcpy(colours[i], ORANGE);
 		}
 		else if (strcmp(arg_v[i + 2], "cyan") == 0) {
 			colours[i] = (char*)malloc(sizeof(CYAN));
-			colours[i] = CYAN;
+			strcpy(colours[i], CYAN);
 		}
 		else if (strcmp(arg_v[i + 2], "magenta") == 0) {
 			colours[i] = (char*)malloc(sizeof(MAGENTA));
-			colours[i] = MAGENTA;
+			strcpy(colours[i], MAGENTA);
 		}
 		else if (strcmp(arg_v[i + 2], "ocean") == 0) {
 			colours[i] = (char*)malloc(sizeof(OCEAN));
-			colours[i] = OCEAN;
+			strcpy(colours[i], OCEAN);
 		}
 		else if (strcmp(arg_v[i + 2], "violet") == 0) {
 			colours[i] = (char*)malloc(sizeof(VIOLET));
-			colours[i] = VIOLET;
+			strcpy(colours[i], VIOLET);
 		}
 		else {
 			printf("Colour %d is invalid\n", i + 1);
@@ -75,17 +75,22 @@ int main(int arg_c, char** arg_v) {
 
 	int pid;
 
+	for (int i = 0; i < 5; i++) {
+		printf("%s\n", colours[i]);
+	}
+
 	for (int i = 0; i < 10; i++) {
-		if ((pid = fork) == 0) {
-			for (int j = 0; j < 100; j++) {
+		if ((pid = fork()) == 0) {
+			for (int j = 0; j < 1000; j++) {
 				for (int k = 0; k < 1000; k++) {
 					if (k < 500) {
-						write(img, colours[1], sizeof(colours[1]));
+						write(img, colours[3], sizeof(colours[3]));
 					}
 					else {
-						write(img, colours[2], sizeof(colours[2]));
+						write(img, colours[4], sizeof(colours[4]));
 					}
 				}
+				write(img, "\n", 1);
 			}
 		}
 	}
