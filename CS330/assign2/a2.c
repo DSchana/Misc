600d5418d6f02cdbf58b8f04b1d4f6bdd19745cf
dschana <dschana6@gmail.com>
2018-01-30 21:05:39 -0500

    CS330 assign2. and then some

--- CS330/assign2/a2.c
+++ CS330/assign2/a2.c
@@ -1,9 +1,37 @@
+/*
+ * Title:	a2.c
+ * Author:	Dilpreet S. Chana
+ * Description: Create zombie process. Kill any parents of
+ *		zombie process.
+**/
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
+	pid_t pid;
+
+	// Parent
+	if ((pid = fork()) > 0) {
+		sleep(10);
+		printf("10 seconds later... what is my child??\n");
+
+		// Another child
+		if ((pid = fork()) == 0) {
+			execlp("ps", "ps", "-l", (char*)NULL);
+		}
+
+		sleep(1);
+		printf("My child is a zombie\n");
+		pause();
+	}
+	else {  // Child
+		exit(0);
+	}
+
 	return 0;
 }
