eca5945956b180581f43b25833b502a71bab86f7
Dilpreet S. Chana <dschana6@gmail.com>
2018-03-04 12:46:40 -0500

    pigpio

--- C++/pigpio/main.cpp
+++ C++/pigpio/main.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
+#include <iostream>
 #include <pigpio.h>
 
 using namespace std;
 
 int main() {
+	cout << "START..." << endl;
 	gpioInitialise();
 
 	gpioSetMode(12, PI_OUTPUT);
@@ -11,12 +13,24 @@ int main() {
 
 	gpioPWM(12, 128);  // Talon OFF
 	time_sleep(5);
+
+	cout << "Ready. Flip switch to on..." << endl;
+	scanf("");
+
+	cout << "LOW..." << endl;
 	gpioPWM(12, 85);  // Talon MAX REV
 	time_sleep(5);
+
+	cout << "HIGH..." << endl;
 	gpioPWM(12, 170);  // Talon MAX FORW
-	time_sleep(5)
+	time_sleep(5);
+
+	cout << "Turning off..." << endl;
 	gpioPWM(12, 128);  // Talon OFF
 	time_sleep(10);
+
+	cout << "DONE. Flip switch to off" << endl;
+	scanf("");
 
 	gpioTerminate();
 
